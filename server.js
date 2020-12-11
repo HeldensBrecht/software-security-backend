d46651181dbe2bef7d7bb64979dcac82918bc7a4
HeldensBrecht <brecht.heldens@student.ehb.be>
2020-12-11 16:01:51 +0100

    Stock toevoegen + store en update calls voor producten

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const jwt = require("express-jwt");
 const jwks = require("jwks-rsa");
 require("dotenv").config();
 
-console.log(process.env.AUTH0_DOMAIN);
 const { PORT = 3001 } = process.env;
 const products = require("./modules/products.js");
 
@@ -26,7 +25,8 @@ const app = express();
 app.use(express.json());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_APP_URL);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
@@ -73,6 +73,13 @@ app.get("/products/:prodId", (req, res) => {
 app.post("/products", validateJwt, (req, res) => {
   products
     .store(req.body)
+    .then((result) => res.send(result))
+    .catch((err) => res.send(err));
+});
+
+app.put("/products/:prodId", validateJwt, (req, res) => {
+  products
+    .update(req.body, req.id)
     .then((result) => res.send(result))
     .catch((err) => res.send(err));
 });
